e52639d42835d20c556f310172aa5288bd218285
Dem1se <danksepticeye@protonmail.com>
2020-05-28 02:44:28 +0530

    initial commit

---
+++ CustomReminders/main.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+using System.IO;
+using StardewValley.Objects;
+
+namespace Dem1se.CustomReminders
+{
+    /// <summary>The mod entry point.</summary>
+    public class ModEntry : Mod
+    {
+        private ModConfig Config;
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides simplified APIs for writing mods.</param>
+        public override void Entry(IModHelper helper)
+        {
+            this.Config = this.Helper.ReadConfig<ModConfig>();
+            
+            // Binds the event with method.
+            helper.Events.Input.ButtonPressed += OnButtonPressed;
+            helper.Events.GameLoop.TimeChanged += ReminderNotifier;
+        }
+
+        private void OnButtonPressed(object sender, ButtonPressedEventArgs ev)
+        {
+            // ignore if player hasn't loaded a save yet
+            if (!Context.IsWorldReady)
+                return;
+            
+            // reminder menu
+            if (ev.Button == this.Helper.Data.ReadJsonFile<ModConfig>("config.json").Button)
+            {
+                // Create new reminder  (testing)
+                //this.Helper.Data.WriteJsonFile($"data/{Constants.SaveFolderName}/reminder1.json", reminder);
+            }
+        }
+        
+        //TODO: make it run every 30 secs and not evey 10 secs
+        private void ReminderNotifier(object sender, TimeChangedEventArgs ev)
+        {
+            string TimeString = Convert.ToString(ev.NewTime);
+            if (!(TimeString.EndsWith("30") || TimeString.EndsWith("00"))) { return; }
+            SDate CurrentDate = SDate.Now();
+            for (int i = 0; i < 20; i++)
+            {
+                try
+                {
+                    this.Monitor.VerboseLog($"Processed {ev.NewTime}");
+                    var Reminder = this.Helper.Data.ReadJsonFile<ReminderModel>($"data/{Constants.SaveFolderName}/reminder{i}.json");
+                    if (Reminder.DaysSinceStart == CurrentDate.DaysSinceStart)
+                    {
+                        if (Reminder.Time == ev.NewTime)
+                        {
+                            this.Monitor.VerboseLog($"Reminder set for {Reminder.DaysSinceStart} on {CurrentDate.DaysSinceStart}");
+                            Game1.addHUDMessage(new HUDMessage(Reminder.ReminderMessage, 2));
+                        }
+                    }
+                }
+                catch (FileNotFoundException)
+                {
+                    break;
+                }
+                catch (NullReferenceException)
+                {
+                    break;
+                }
+            }
+        }
+    }
+
+    /// <summary> Data model for reminders </summary>
+    class ReminderModel
+    {
+        public string ReminderMessage { get; set; }
+        public int DaysSinceStart { get; set; }
+        public int Time { get; set; }
+    }
+    
+    /// <summary> Mod config.json data model </summary>
+    class ModConfig
+    {
+        public SButton Button { get; set; } = SButton.F2;
+    }
+}
---
+++ ReminderGenerator/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace ReminderGenerator
+{
+    class Program
+    {
+        static void Main()
+        {
+            string command = "";
+            // Main loop
+            do
+            {
+                Console.WriteLine("Commands:\nnew - new reminder_number{int} date{int} season{int} year{int} time{int} message{string}\nhelp\nquit");
+                command = Console.ReadLine();
+                CommandHandler(command);
+            } while (!command.ToLower().StartsWith("quit"));
+        }
+
+        static private void CommandHandler(string command)
+        {
+            string[] command_parts = command.Split(' ');
+            switch (command_parts[0].ToLower())
+            {
+                case "new":
+                    {
+                        string ReminderMessage = "";
+                        int DaysSinceStart, Time;
+
+                        DaysSinceStart = ConvertToDays(command_parts[2], command_parts[3], command_parts[4]);
+                        Time = Convert.ToInt32(command_parts[5]);
+                        for (int i = 0; i < command_parts.Length - 6; i++)
+                        {
+                            int index = i + 6;
+                            ReminderMessage += " " + command_parts[index];
+                        }
+                        WriteToFile(command_parts[1], ReminderMessage, DaysSinceStart, Time);
+                        break;
+                    }
+                case "help":
+                    Console.WriteLine("Create new reminders using the 'new' command.\nExit the application using 'quit' command.\nThis program generates new json files for the mod to process.\n");
+                    break;
+                case "quit":
+                    break;
+                default:
+                    Console.WriteLine("Invalid commands");
+                    break;
+            }
+
+        }
+        
+        static private void WriteToFile(string reminder_num, string ReminderMessage, int DaysSinceStart, int Time)
+        {
+            var options = new JsonWriterOptions { Indented = true };
+            using (var stream = new MemoryStream())
+            {
+                using (var writer = new Utf8JsonWriter(stream, options))
+                {
+                    writer.WriteStartObject();
+                    writer.WriteString("ReminderMessage", ReminderMessage);
+                    writer.WriteNumber("DaysSinceStart", DaysSinceStart);
+                    writer.WriteNumber("Time", Time);
+                    writer.WriteEndObject();
+                }
+                string json = Encoding.UTF8.GetString(stream.ToArray());
+                System.IO.File.WriteAllText($"C:\\Program Files (x86)\\Steam\\steamapps\\common\\Stardew Valley\\Mods\\CustomReminders\\data\\Wolfie_183009987\\reminder{reminder_num}.json", json);
+            }
+        }
+
+        static private int ConvertToDays(string date, string season, string year)
+        {
+            int DaysSinceStart = ((Convert.ToInt32(season) - 1) * 28) + ((Convert.ToInt32(year) - 1) * 112) + Convert.ToInt32(date) + 28;
+            return DaysSinceStart;
+        }
+    }
+}
