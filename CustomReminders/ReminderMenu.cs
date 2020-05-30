9d97ae684ea00b73ac7bde431647be759cce6cf8
Dem1se <danksepticeye@protonmail.com>
2020-05-31 05:29:45 +0530

    Draft implementation of the UI refactored and rewrote some parts to fit new structure.

---
+++ CustomReminders/ReminderMenu.cs
@@ -0,0 +1,185 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using StardewValley;
+using StardewValley.BellsAndWhistles;
+using StardewValley.Menus;
+using System;
+
+namespace Dem1se.CustomReminders
+{
+    public class ReminderMenu : IClickableMenu
+    {
+        protected int minLength = 1;
+        public const int region_okButton = 101;
+        public const int region_doneNamingButton = 102;
+        //public const int region_randomButton = 103;
+        public const int region_textBox_ReminderMessage = 104;
+        public const int region_textBox_DateAndTime = 105;
+        public ClickableTextureComponent doneNamingButton;
+        protected TextBox textBox_ReminderMessage;
+        protected TextBox textBox_DateAndTime;
+        public ClickableComponent textBoxCC_ReminderMessage;
+        public ClickableComponent textBoxCC_DateAndTime;
+        private TextBoxEvent e;
+        private ReminderMenu.doneNamingBehavior doneNaming;
+        private string title;
+
+        public ReminderMenu(ReminderMenu.doneNamingBehavior b, string title, string defaultName = null)
+        {
+            this.doneNaming = b;
+            this.xPositionOnScreen = 0;
+            this.yPositionOnScreen = 0;
+            this.width = Game1.viewport.Width;
+            this.height = Game1.viewport.Height;
+            this.title = title;
+
+            this.textBox_ReminderMessage = new TextBox((Texture2D)null, (Texture2D)null, Game1.smallFont, Game1.textColor);
+            this.textBox_ReminderMessage.X = Game1.viewport.Width / 2 - 192;
+            this.textBox_ReminderMessage.Y = Game1.viewport.Height / 2;
+            this.textBox_ReminderMessage.Width = 384;
+            this.textBox_ReminderMessage.Height = 192;
+
+            this.textBox_DateAndTime = new TextBox((Texture2D)null, (Texture2D)null, Game1.dialogueFont, Game1.textColor);
+            this.textBox_DateAndTime.X = Game1.viewport.Width / 2 - 192;
+            this.textBox_DateAndTime.Y = (Game1.viewport.Height / 2) + 96;
+            this.textBox_DateAndTime.Width = 384;
+            this.textBox_DateAndTime.Height = 192;
+
+            this.e = new TextBoxEvent(this.textBoxEnter);
+            this.textBox_DateAndTime.OnEnterPressed += this.e;
+            Game1.keyboardDispatcher.Subscriber = (IKeyboardSubscriber)this.textBox_ReminderMessage;
+
+            this.textBox_ReminderMessage.Text = "Reminder message";
+            this.textBox_ReminderMessage.Selected = true;
+
+            this.textBox_DateAndTime.Text = "12 summer 1400";
+            this.textBox_DateAndTime.Selected = false;
+
+            // Random Button
+            //ClickableTextureComponent textureComponent1 = new ClickableTextureComponent(new Rectangle(this.textBox_ReminderMessage.X + this.textBox_ReminderMessage.Width + 64 + 48 - 8, Game1.viewport.Height / 2 + 4, 64, 64), Game1.mouseCursors, new Rectangle(381, 361, 10, 10), 4f, false);
+            //textureComponent1.myID = 103;
+            //textureComponent1.leftNeighborID = 102;
+
+            // Done naming button
+            ClickableTextureComponent textureComponent2 = new ClickableTextureComponent(new Rectangle(this.textBox_DateAndTime.X + this.textBox_DateAndTime.Width + 32 + 4, Game1.viewport.Height / 2 - 8, 64, 64), Game1.mouseCursors, Game1.getSourceRectForStandardTileSheet(Game1.mouseCursors, 46, -1, -1), 1f, false);
+            textureComponent2.myID = 102;
+            //textureComponent2.rightNeighborID = 103;
+            textureComponent2.leftNeighborID = 105;
+            this.doneNamingButton = textureComponent2;
+
+            this.textBoxCC_ReminderMessage = new ClickableComponent(new Rectangle(this.textBox_ReminderMessage.X, this.textBox_ReminderMessage.Y, 384, 48), "")
+            {
+                myID = 104
+            };
+            this.textBoxCC_DateAndTime = new ClickableComponent(new Rectangle(this.textBox_DateAndTime.X, this.textBox_DateAndTime.Y, 384, 48), "")
+            {
+                myID = 105,
+                rightNeighborID = 102
+            };
+
+            if (!Game1.options.SnappyMenus)
+                return;
+            this.populateClickableComponentList();
+            this.snapToDefaultClickableComponent();
+        }
+
+        public override void snapToDefaultClickableComponent()
+        {
+            this.currentlySnappedComponent = this.getComponentWithID(104);
+            this.snapCursorToCurrentSnappedComponent();
+        }
+
+        public void textBoxEnter(TextBox sender)
+        {
+            if (sender.Text.Length < this.minLength)
+                return;
+            if (this.doneNaming != null)
+            {
+                if (sender == textBox_ReminderMessage)
+                {
+                    // TODO:switch to second field
+                    this.textBox_ReminderMessage.Selected = false;
+                    this.textBox_DateAndTime.Selected = true;
+
+                }
+                else if (sender == textBox_DateAndTime)
+                {
+                    this.doneNaming(textBox_ReminderMessage.Text, textBox_DateAndTime.Text);
+                }
+            }
+            else
+                Game1.exitActiveMenu();
+        }
+
+        public override void receiveGamePadButton(Buttons b)
+        {
+            base.receiveGamePadButton(b);
+            if (!this.textBox_ReminderMessage.Selected)
+                return;
+            switch (b)
+            {
+                case Buttons.DPadUp:
+                case Buttons.DPadDown:
+                case Buttons.DPadLeft:
+                case Buttons.DPadRight:
+                case Buttons.LeftThumbstickLeft:
+                case Buttons.LeftThumbstickUp:
+                case Buttons.LeftThumbstickDown:
+                case Buttons.LeftThumbstickRight:
+                    this.textBox_ReminderMessage.Selected = false;
+                    break;
+            }
+        }
+
+        public override void receiveKeyPress(Keys key)
+        {
+            if (this.textBox_ReminderMessage.Selected || Game1.options.doesInputListContain(Game1.options.menuButton, key))
+                return;
+            base.receiveKeyPress(key);
+        }
+
+        public override void receiveRightClick(int x, int y, bool playSound = true)
+        {
+        }
+
+        public override void performHoverAction(int x, int y)
+        {
+            base.performHoverAction(x, y);
+            if (this.doneNamingButton != null)
+            {
+                if (this.doneNamingButton.containsPoint(x, y))
+                    this.doneNamingButton.scale = Math.Min(1.1f, this.doneNamingButton.scale + 0.05f);
+                else
+                    this.doneNamingButton.scale = Math.Max(1f, this.doneNamingButton.scale - 0.05f);
+            }
+        }
+
+        public override void receiveLeftClick(int x, int y, bool playSound = true)
+        {
+            base.receiveLeftClick(x, y, playSound);
+            this.textBox_ReminderMessage.Update();
+            this.textBox_DateAndTime.Update();
+            if (this.doneNamingButton.containsPoint(x, y))
+            {
+                this.textBoxEnter(this.textBox_DateAndTime);
+                Game1.playSound("smallSelect");
+            }
+            this.textBox_ReminderMessage.Update();
+            this.textBox_DateAndTime.Update();
+        }
+
+        public override void draw(SpriteBatch b)
+        {
+            base.draw(b);
+            b.Draw(Game1.fadeToBlackRect, Game1.graphics.GraphicsDevice.Viewport.Bounds, Color.Black * 0.75f);
+            SpriteText.drawStringWithScrollCenteredAt(b, this.title, Game1.viewport.Width / 2, Game1.viewport.Height / 2 - 128, this.title, 1f, -1, 0, 0.88f, false);
+            this.textBox_ReminderMessage.Draw(b, true);
+            this.textBox_DateAndTime.Draw(b, true);
+            this.doneNamingButton.draw(b);
+            this.drawMouse(b);
+        }
+
+        public delegate void doneNamingBehavior(string RemMsg, string DateTime);
+    }
+}
--- CustomReminders/main.cs
+++ CustomReminders/main.cs
@@ -1,46 +1,88 @@
-﻿using System;
+﻿using Newtonsoft.Json;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using StardewModdingAPI.Utilities;
 using StardewValley;
+using StardewValley.Menus;
+using System;
 using System.IO;
-using StardewValley.Objects;
 
 namespace Dem1se.CustomReminders
 {
     /// <summary>The mod entry point.</summary>
     public class ModEntry : Mod
     {
+        /// <summary>
+        /// This is the delegate that defines what happens after the naming is complete.
+        /// </summary>
+        public ReminderMenu.doneNamingBehavior DoneNaming;
         /// <summary> Object with all the properties of the config.</summary>
         private ModConfig Config;
-        
+
         /// <summary>The mod entry point, called after the mod is first loaded.</summary>
         /// <param name="helper">Provides simplified APIs for writing mods.</param>
         public override void Entry(IModHelper helper)
         {
             // Load the Config
             this.Config = this.Helper.ReadConfig<ModConfig>();
-            
+
             // Binds the event with method.
             helper.Events.Input.ButtonPressed += OnButtonPressed;
             helper.Events.GameLoop.TimeChanged += ReminderNotifier;
 
+            this.DoneNaming = new ReminderMenu.doneNamingBehavior(AfterNaming);
         }
-        
+
+        /// <summary>
+        /// Parse and Store the reminder data from the reminder menu
+        /// </summary>
+        /// <param name="ReminderMessage">This is the input from the textbox.</param>
+        public void AfterNaming(string RemMsg, string DateTime)
+        {
+            Monitor.Log(RemMsg + DateTime, LogLevel.Trace);
+            Game1.exitActiveMenu();
+
+            // separate the date and time
+            string[] DateAndTime = DateTime.Split(' ');
+            string Date = DateAndTime[0];
+            int Season = 0;
+            switch (DateAndTime[1].ToLower())
+            {
+                case "summer":
+                    Season = 1;
+                    break;
+                case "spring":
+                    Season = 2;
+                    break;
+                case "fall":
+                    Season = 3;
+                    break;
+                case "winter":
+                    Season = 4;
+                    break;
+            }
+            int Time = Convert.ToInt32(DateAndTime[2]);
+
+            // covert the date to dayssincestart
+            int DaysSince = ConvertToDays(Date, Season);
+            // write the data to file
+
+            WriteToFile(RemMsg, DaysSince, Time);
+        }
+
+
         /// <summary> Defines what happens when user press the config button </summary>
         private void OnButtonPressed(object sender, ButtonPressedEventArgs ev)
         {
             // ignore if player hasn't loaded a save yet
             if (!Context.IsWorldReady)
                 return;
-            
-            // reminder menu
-            if (ev.Button == Config.Button)
-            {
-                // new reminder menu
-            }
+
+            if (Game1.activeClickableMenu != null || (!Context.IsPlayerFree) || ev.Button != Config.Button) { return; }
+            Game1.activeClickableMenu = (IClickableMenu)new ReminderMenu(DoneNaming, "New Reminder");
+
         }
-        
+
         /// <summary> Loop that checks if </summary>
         private void ReminderNotifier(object sender, TimeChangedEventArgs ev)
         {
@@ -49,12 +91,13 @@ namespace Dem1se.CustomReminders
             if (!(TimeString.EndsWith("30") || TimeString.EndsWith("00"))) { return; }
 
             // Loops through all the reminder files and evaluates if they are current.
-            #region CoreEvaluationLoop
+            #region CoreReminderLoop
             SDate CurrentDate = SDate.Now();
             foreach (string FilePathAbsolute in Directory.EnumerateFiles($"{Constants.ExecutionPath}\\Mods\\CustomReminders\\data\\{Constants.SaveFolderName}"))
             {
                 try
                 {
+                    // Make relative path from absolute path
                     string[] FilePathAbsoulute_Parts = FilePathAbsolute.Split('\\');
                     string FilePathRelative = "";
                     int FilePathIndex = Array.IndexOf(FilePathAbsoulute_Parts, "data");
@@ -62,10 +105,11 @@ namespace Dem1se.CustomReminders
                     {
                         FilePathRelative += FilePathAbsoulute_Parts[i] + "\\";
                     }
-                    
+
                     // Remove the trailing forward slash in Relative path
                     FilePathRelative = FilePathRelative.Remove(FilePathRelative.LastIndexOf("\\"));
 
+                    // Read the reminder and notify if mature
                     this.Monitor.Log($"Processed {ev.NewTime}", LogLevel.Trace);
                     var Reminder = this.Helper.Data.ReadJsonFile<ReminderModel>(FilePathRelative);
                     if (Reminder.DaysSinceStart == CurrentDate.DaysSinceStart)
@@ -85,19 +129,56 @@ namespace Dem1se.CustomReminders
             }
             #endregion
         }
-    }
 
-    /// <summary> Data model for reminders </summary>
-    class ReminderModel
-    {
-        public string ReminderMessage { get; set; }
-        public int DaysSinceStart { get; set; }
-        public int Time { get; set; }
-    }
-    
-    /// <summary> Mod config.json data model </summary>
-    class ModConfig
-    {
-        public SButton Button { get; set; } = SButton.F2;
+        /// <summary>
+        /// This function will write the reminder to the json file reliably.
+        /// </summary>
+        /// <param name="ReminderMessage">The message that will pop up in reminder</param>
+        /// <param name="DaysSinceStart">The date converted to DaysSinceStart</param>
+        /// <param name="Time">The time of the reminder in 24hrs format</param>
+        private static void WriteToFile(string ReminderMessage, int DaysSinceStart, int Time)
+        {
+            ReminderModel ReminderData = new ReminderModel()
+            { 
+                DaysSinceStart = DaysSinceStart, 
+                ReminderMessage = ReminderMessage, 
+                Time = Time 
+            };
+            string SerializedReminderData = JsonConvert.SerializeObject(ReminderData, Formatting.Indented);
+            int ReminderCount = 0;
+            foreach (string Path in Directory.EnumerateFiles($"{Constants.ExecutionPath}\\mods\\CustomReminders\\data\\{Constants.SaveFolderName}"))
+            {
+                ReminderCount++;
+            }
+            System.IO.File.WriteAllText($"{Constants.ExecutionPath}\\mods\\CustomReminders\\data\\{Constants.SaveFolderName}\\reminder{++ReminderCount}.json", SerializedReminderData);
+        }
+
+        private static int ConvertToDays(string date, int season, string year)
+        {
+            int DaysSinceStart = ((Convert.ToInt32(season) - 1) * 28) + ((Convert.ToInt32(year) - 1) * 112) + Convert.ToInt32(date) + 28;
+            return DaysSinceStart;
+        }
+
+        private static int ConvertToDays(string date, int season)
+        {
+            int year = SDate.Now().Year;
+            int DaysSinceStart = ((season - 1) * 28) + ((year - 1) * 112) + Convert.ToInt32(date) + 28;
+            return DaysSinceStart;
+        }
     }
 }
+
+/// <summary> Data model for reminders </summary>
+class ReminderModel
+{
+    public string ReminderMessage { get; set; }
+    public int DaysSinceStart { get; set; }
+    public int Time { get; set; }
+}
+
+/// <summary> Mod config.json data model </summary>
+class ModConfig
+{
+    public SButton Button { get; set; } = SButton.F2;
+}
+
